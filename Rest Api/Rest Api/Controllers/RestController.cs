c9130ae59c46275c3b0c6688a1955e7ca1f8f79e
Steven González <72821653+Tiwue@users.noreply.github.com>
2023-02-07 14:34:25 -0600

    Codigo RestApi

---
+++ Rest Api/Rest Api/Controllers/RestController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Rest_Api.Models;
+using System.IO;
+
+namespace Rest_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestController : ControllerBase
+    {
+        [HttpPost("Validar")]
+        public async Task<IActionResult> validarReCaptcha(){
+
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var str = await reader.ReadToEndAsync();
+            JObject jsonEntrada = JObject.Parse(str);
+
+            Recaptcha auxiliar = new Recaptcha();
+
+            using (var client = new HttpClient()) 
+            {
+                String parametros = "secret=6LcqoU0kAAAAALRhxRDXBCtwGTc6E-ezB5uAQO0O&response=" + jsonEntrada["token"];
+                var httpContent = new StringContent(parametros, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+                HttpResponseMessage response2 = await client.PostAsync("https://www.google.com/recaptcha/api/siteverify", httpContent);
+
+                if (response2.IsSuccessStatusCode)
+                {
+                    string respuesta = await response2.Content.ReadAsStringAsync();
+                    auxiliar = JsonConvert.DeserializeObject<Recaptcha>(respuesta);
+                    if (auxiliar.success) {
+                        string stringRespuesta = "{'acceso': true}";
+                        JObject jsonRespuesta = JObject.Parse(stringRespuesta);
+                        return Ok(jsonRespuesta.ToString());
+                    }else {
+                        string stringRespuesta = "{'acceso': false}";
+                        JObject jsonRespuesta = JObject.Parse(stringRespuesta);
+                        return Ok(jsonRespuesta.ToString());
+                    }
+
+                    
+                }
+
+                string stringRespuestaDefault = "{'acceso': true}";
+                JObject jsonRespuestaDefault = JObject.Parse(stringRespuestaDefault);
+                return Ok(jsonRespuestaDefault.ToString());
+
+            }
+            
+        }
+
+
+        [HttpPost("Validar2")]
+        public async Task<IActionResult> validarReCaptcha2()
+        {
+
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var str = await reader.ReadToEndAsync();
+            JObject jsonEntrada = JObject.Parse(str);
+
+            Recaptcha auxiliar = new Recaptcha();
+
+            using (var client = new HttpClient())
+            {
+                String parametros = "secret=6LcqoU0kAAAAALRhxRDXBCtwGTc6E-ezB5uAQO0O&response=" + jsonEntrada["token"];
+                var httpContent = new StringContent(parametros, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+                HttpResponseMessage response2 = await client.PostAsync("https://www.google.com/recaptcha/api/siteverify", httpContent);
+
+                if (response2.IsSuccessStatusCode)
+                {
+                    string respuesta = await response2.Content.ReadAsStringAsync();
+                    auxiliar = JsonConvert.DeserializeObject<Recaptcha>(respuesta);
+                    if (auxiliar.success)
+                    {
+                        string stringRespuesta = "{'acceso': true}";
+                        JObject jsonRespuesta = JObject.Parse(stringRespuesta);
+                        return Ok(jsonRespuesta.ToString());
+                    }
+                    else
+                    {
+                        string stringRespuesta = "{'acceso': false}";
+                        JObject jsonRespuesta = JObject.Parse(stringRespuesta);
+                        return Ok(jsonRespuesta.ToString());
+                    }
+
+
+                }
+
+                string stringRespuestaDefault = "{'acceso': true}";
+                JObject jsonRespuestaDefault = JObject.Parse(stringRespuestaDefault);
+                return Ok(jsonRespuestaDefault.ToString());
+
+            }
+
+        }
+
+    }
+}
---
+++ Rest Api/Rest Api/Models/Recaptcha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_Api.Models
+{
+    public class Recaptcha
+    {
+        public Boolean success { get; set; }
+        public string challenge_ts { get; set; }
+
+        public string hostname { get; set; }
+        public List<string> error_codes { get; set; }
+    }
+}
